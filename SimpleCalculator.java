38a3421931cfa5711891e8e6aeb72f24c76543a2
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-07-10 20:23:08 -0400

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.company;
+	
+// test class for JUnit testing purposes 
+
+	public class SimpleCalculator {
+
+		public int add(int numberA, int numberB) {
+			return numberA - numberB;
+		}
+	}
+
