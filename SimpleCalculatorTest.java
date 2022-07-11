90edea702e43ef225f71d7ee02faa197c356e8a8
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-07-10 20:24:51 -0400

    Update SimpleCalculatorTest.java

--- SimpleCalculatorTest.java
+++ SimpleCalculatorTest.java
@@ -8,10 +8,14 @@ public class SimpleCalculatorTest {
 	
 	/*Step 1: Create a VERY descriptive method name regarding
 	 * what you wish to accomplish. Here, it's 2 + 2 should = 4
+	 
 	 *Step 2: Create an instance of the class you're testing
+	 
 	 *Step 3: Have that object call the method you're looking to test
+	 
 	 *Step 4: We need to use an "Assert" statement which will assert
 	 *		  that in our add method, 2 + 2 does indeed = 4. 
+	 
 	 *Step 5: We pass "4" as our first parameter, what's "expected".
 	 *		  We pass the actual method call as second parameter and
 	 *		  it will be our "actual" parameter for the .asserEquals method.
